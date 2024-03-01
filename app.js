49b119efe8a9294ff2955cf7eaf1df1ccdb51b4c
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2024-03-01 18:42:03 -0300

    Create a new template, a Cost Manager app.

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('home.html', { root: 'views' });
+})
+
+app.listen(port, (req, res) => {
+  console.log(`Server running on port http://localhost:${port}`)
+})
