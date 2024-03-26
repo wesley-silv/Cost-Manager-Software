9966dd545bb40ce442f68bfd1b65e2664adf30b2
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2024-03-26 17:56:27 -0300

    Form not attend the call of route API.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var app = express();
-var host = 'localhost';
-var port = 3000;
-app.use(express.static('public'));
+var express = require('express')
+var app = express()
+var host = 'localhost'
+var port = 3000
+app.use(express.static('public'))
 app.get('/', function (req, res) {
-    res.sendFile('home.html', { root: 'views' });
-});
+  res.sendFile('home.html', { root: 'views' })
+})
 app.listen(port, function (req, res) {
-    console.log("Server running on port http://".concat(host, ":").concat(port));
-});
+  console.log('Server running on port http://'.concat(host, ':').concat(port))
+})
--- app.ts
+++ app.ts
@@ -1,14 +1,25 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express()
 const host = 'localhost'
 const port = 3000
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
   res.sendFile('home.html', { root: 'views' });
 })
 
+app.post('/insert-dates', (req, res) => {
+  const costsFixed = req.body.costs_fixed;
+  const costsNotFixed = req.body.costs_not_fixed;
+
+  res.send(`Dados recebidos: Costs Fixed = ${costsFixed}, Costs Not Fixed = ${costsNotFixed}`);
+
+})
 app.listen(port, (req, res) => {
   console.log(`Server running on port http://${host}:${port}`)
 })
