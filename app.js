8972ba9a362814d00c809304036ecf123e937051
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2024-04-04 14:32:53 -0300

    Update the API route to render the HTML list file.

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-var express = require('express')
-var app = express()
-var host = 'localhost'
-var port = 3000
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const host = 'localhost'
+const port = 3000
+
 app.use(express.static('public'))
-app.get('/', function (req, res) {
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
   res.sendFile('home.html', { root: 'views' })
 })
-app.listen(port, function (req, res) {
-  console.log('Server running on port http://'.concat(host, ':').concat(port))
+
+app.get('/list', (req, res) => {
+  res.sendFile('list.html', { root: 'views' })
+})
+
+app.post('/insert-dates', (req, res) => {
+  const costsFixed = req.dates.costs_fixed
+  const costsNotFixed = req.body.costs_not_fixed
+  console.log(costsFixed)
+  res.send(
+    `Dados recebidos: Costs Fixed = ${costsFixed}, Costs Not Fixed = ${costsNotFixed}`
+  )
+})
+app.listen(port, (req, res) => {
+  console.log(`Server running on port http://${host}:${port}`)
 })
